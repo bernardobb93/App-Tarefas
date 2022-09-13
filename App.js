6563b5f275457981659a2daef864478ba4c9fa1c
bernardobb <bernardobb.eletronica@gmail.com>
2022-09-13 15:53:50 -0300

    finalizado com erro

--- App.js
+++ App.js
@@ -1,132 +1,193 @@
-import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AppLoading } from 'expo';
+import { AntDesign } from '@expo/vector-icons'; 
 import { useFonts, Lato_400Regular } from '@expo-google-fonts/lato';
-import { Ionicons } from '@expo/vector-icons';
-import { StyleSheet, Text,Modal,TouchableHighlight, View, ImageBackground, TouchableOpacity, ScrollView, TextInput } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity,TouchableHighlight ,Modal ,ScrollView, TextInput } from 'react-native';
 
 export default function App() {
 
-  const image = require("./resources/bg.jpg");
-  console.disableYellowBox=true;
-  const [tarefas, setarTarefas] = useState([
-    {
-    id:1,
-    tarefa:'Minha tarefa 1'
-  },
-  {
-    id:2,
-    tarefa:'Minha tarefa 2'
-  },
-  {
-    id:3,
-    tarefa:'Minha tarefa 3'
-  }
-]);
+  const image = require('./resources/bg.jpg');
 
- const [modal,setModal]=useState(true);
+  console.disableYellowBox = true;
 
+  const [tarefas, setarTarefas] = useState([]);
+
+  const [modal,setModal] = useState(false);
+
+  const [tarefaAtual,setTarefaAtual] = useState('');
+
+
+ 
   let [fontsLoaded] = useFonts({
     Lato_400Regular,
   });
 
+  useEffect(()=>{
+    //alert('app carregado...');
+    
+    (async () => {
+      try {
+        let tarefasAtual = await AsyncStorage.getItem('tarefas');
+        if(tarefasAtual == null)
+          setarTarefas([]);
+        else
+          setarTarefas(JSON.parse(tarefasAtual));
+      } catch (error) {
+        // Error saving data
+      }
+    })();
+    
+},[])
+
   if (!fontsLoaded) {
-    return null;
+    return <AppLoading />;
   }
+
+ 
 
   function deletarTarefa(id){
-    alert('Tarefa foi deletado com sucesso!')
-    let newTarefas=tarefas.filter(function(val){
-      return val.id!=id;
-    });
-    setarTarefas(newTarefas);
+      alert('Tarefa com id '+id+' foi deletada com sucesso!');
+      //TODO: Deletar do array/estado a tarefa com id especificado!
+      let newTarefas = tarefas.filter(function(val){
+            return val.id != id;
+      });
+
+      setarTarefas(newTarefas);
+     
+      (async () => {
+        try {
+          await AsyncStorage.setItem('tarefas', JSON.stringify(newTarefas));
+          //console.log('chamado');
+        } catch (error) {
+          // Error saving data
+        }
+      })();
+      
   }
 
+  function addTarefa(){
+    
+    setModal(!modal);
+
+    let id = 0;
+    if(tarefas.length > 0){
+        id = tarefas[tarefas.length-1].id + 1;
+    }
+
+    let tarefa = {id:id,tarefa:tarefaAtual};
+
+    setarTarefas([...tarefas,tarefa]);
+
+   
+
+    (async () => {
+      try {
+        await AsyncStorage.setItem('tarefas', JSON.stringify([...tarefas,tarefa]));
+      } catch (error) {
+        // Error saving data
+      }
+    })();
+    
+  }
+
+ 
+
   return (
+    
     <ScrollView style={{flex:1}}>
-      <StatusBar hidden/>
+      <StatusBar hidden />
       <Modal
         animationType="slide"
         transparent={true}
         visible={modal}
         onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-        }}>
-         
+          Alert.alert("Modal has been closed.");
+        }}
+      >
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <TextInput autoFocus={true}></TextInput>
+            <TextInput onChangeText={text => setTarefaAtual(text)} autoFocus={true}></TextInput>
+
             <TouchableHighlight
-              style={{...styles.openButton,backgroundColor:'#2196F3'}}
-              onPress={() => {
-                setModal(!modal);
-              }}>
-            <Text style={styles.textStyle}>Adicionar Tarefa</Text>
+              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+              onPress={() => addTarefa()}
+            >
+              <Text style={styles.textStyle}>Adicionar Tarefa</Text>
             </TouchableHighlight>
           </View>
         </View>
       </Modal>
 
+      
+
         <ImageBackground source={image} style={styles.image}>
-        <View style={styles.coverView}>
-          <Text style={styles.textHeader}>Lista de Tarefas</Text>
-          </View>
+          <View style={styles.coverView}>
+            <Text style={styles.textHeader}>Lista de Tarefas - Danki Code</Text>
+            </View>
         </ImageBackground>
+
+      
+
         {
-          tarefas.map(function(val){
-        return(<View style={styles.tarefaSingle}>
-          <View style={{flex:1,width:'100%',padding:10}}>
-            <Text>{val.tarefa}</Text>
-          </View>
-          <View style={{alignItems:'flex-end',flex:1, padding:10}}>
-            <TouchableOpacity onPress={()=>deletarTarefa(val.id)}>
-              <Ionicons name="trash-outline" size={24} color="black" />
-              </TouchableOpacity>
-          </View>
-        </View>);
-})
-}
-<TouchableOpacity style={styles.btnAddTarefa} onPress={()=>setModal(true)}><Text
+        tarefas.map(function(val){
+          return (<View style={styles.tarefaSingle}>
+            <View style={{flex:1,width:'100%',padding:10}}>
+                <Text>{val.tarefa}</Text>
+            </View>
+            <View style={{alignItems:'flex-end',flex:1,padding:10}}>
+              <TouchableOpacity onPress={()=> deletarTarefa(val.id)}><AntDesign name="minuscircleo" size={24} color="black" /></TouchableOpacity>
+            </View>
+            </View>);
+        })
+        
+
+        }
+
+        <TouchableOpacity style={styles.btnAddTarefa} onPress={()=>setModal(true)}><Text
          style={{textAlign:'center',color:'white'}}>Adicionar Tarefa!
          </Text>
          </TouchableOpacity>
-  </ScrollView>
+        
+        </ScrollView>
+       
   );
 }
 
 const styles = StyleSheet.create({
-  
   image: {
     width:'100%',
-    height:90,
-    resizeMode:"cover",
+    height: 80,
+    resizeMode: "cover"
   },
   btnAddTarefa:{
     width:200,
     padding:8,
     backgroundColor:'gray',
-    marginTop:20,
-    marginLeft:20,
+    marginTop:20
   },
   coverView:{
     width:'100%',
-    height:90,
-    backgroundColor:'rgba(0,0,0,0.3)',
+    height:80,
+    backgroundColor:'rgba(0,0,0,0.5)'
   },
   textHeader:{
     textAlign:'center',
     color:'white',
     fontSize:20,
     marginTop:30,
-    fontFamily: 'Lato_400Regular',
+    fontFamily:'Lato_400Regular'
   },
   tarefaSingle:{
-    marginTop:30,
-    width:'100%',
-    borderBottomWidth:1,
-    borderBottomColor:'black',
-    flexDirection:'row',
-    paddingBottom:10,
+      marginTop:30,
+      width:'100%',
+      borderBottomWidth:1,
+      borderBottomColor:'black',
+      flexDirection:'row',
+      paddingBottom:10
   },
+  //Estilos para nossa modal
   centeredView: {
     flex: 1,
     justifyContent: "center",
